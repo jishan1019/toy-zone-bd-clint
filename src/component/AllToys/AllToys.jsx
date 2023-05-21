f2bc015b5495ce8d8067827b0103a19871c91c21
jishan1019 <imranislamjishan200@gmail.com>
2023-05-21 13:21:44 +0600

    all toys table design done

--- src/component/AllToys/AllToys.jsx
+++ src/component/AllToys/AllToys.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import useDynamicTitle from "../CustomHook/useDynamicTitle";
+import AllToysTableBody from "./AllToysTableBody";
 
 const AllToys = () => {
   useDynamicTitle("Toy Zone | All Toy");
-  return <div>all Toys</div>;
+  return (
+    <div>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Toy Name</th>
+              <th>Seller</th>
+              <th>Sub-category</th>
+              <th>Price</th>
+              <th>Available Quantity</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <AllToysTableBody />
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllToys;
