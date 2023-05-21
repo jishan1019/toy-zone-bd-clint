a2b19b4d2bdf1defcc4e4a7ec2b98cebc7b6aec6
jishan1019 <imranislamjishan200@gmail.com>
2023-05-21 13:37:43 +0600

    search by name add on all toys

--- src/component/AllToys/AllToys.jsx
+++ src/component/AllToys/AllToys.jsx
@@ -7,6 +7,19 @@ const AllToys = () => {
   return (
     <div>
       <div className="overflow-x-auto w-full">
+        <div className="mt-8 mb-8 ml-4 mr-4">
+          <div className="flex">
+            <input
+              className="p-4 border-2 border-gray-400 flex-grow rounded-md"
+              type="text"
+              placeholder="Search By Name......."
+            />
+            <button className="p-4 bg-orange-600 -ml-2 text-white font-semibold rounded-md border-t-2 border-r-2 border-b-2 border-gray-400">
+              Search
+            </button>
+          </div>
+        </div>
+
         <table className="table w-full">
           {/* head */}
           <thead>
